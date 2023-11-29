98428b83ec9bc8f055bc85ae1cf7c590c73a3ee6
Satyaprasadjena1 <152400066+Satyaprasadjena1@users.noreply.github.com>
2023-11-29 19:18:03 +0530

    factorial of a number

--- Hello.java
+++ Hello.java
@@ -1,1 +1,17 @@
-
+public class Hello{
+    public static void main(String args[]){
+        int fristnum;
+        int fact = 1;
+        int counter;
+        System.out.println("enter a number");
+        Scanner sc = new Scanner();
+        fristnum = sc.nextInt();
+        counter = fristnum;
+        while (counter >= 1)
+        {
+            fact = fact*1;
+            counter--;
+        }
+        System.out.println(fact);        
+    }
+}
