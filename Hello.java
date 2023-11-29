5aca3b4353591cb04c85d0a6d8b45f756138c602
Satyaprasadjena1 <152400066+Satyaprasadjena1@users.noreply.github.com>
2023-11-29 19:42:49 +0530

    FACTORIAL WITH BIG INT

--- Hello.java
+++ Hello.java
@@ -1,15 +1,17 @@
+import java.util.*;
+import java.math.BigInteger; 
 public class Hello{
     public static void main(String args[]){
         int fristnum;
-        int fact = 1;
+        BigInteger fact = BigInteger.ONE;
         int counter;
         System.out.println("enter a number");
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
         fristnum = sc.nextInt();
         counter = fristnum;
-        while (counter >= 1)
+        while(counter >= 1)
         {
-            fact = fact*1;
+            fact = fact.multiply(BigInteger.valueOf(counter));
             counter--;
         }
         System.out.println(fact);        
